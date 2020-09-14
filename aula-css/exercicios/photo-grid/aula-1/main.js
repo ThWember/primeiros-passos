22fb8a596c59891a4aeb6dd1d043e9afe5e9b8cb
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-14 11:30:18 -0300

    Details function

---
+++ aula-css/exercicios/photo-grid/aula-1/main.js
@@ -0,0 +1,23 @@
+function details1() {
+    window.location.href ="details-1.html";
+}
+
+function details2() {
+    window.location.href ="details-2.html";
+}
+
+function details3() {
+    window.location.href ="details-3.html";
+}
+
+function details4() {
+    window.location.href ="details-4.html";
+}
+
+function details5() {
+    window.location.href ="details-5.html";
+}
+
+function details6() {
+    window.location.href ="details-6.html";
+}
